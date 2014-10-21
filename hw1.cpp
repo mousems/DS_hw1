2e955ae5b377a2b79349dcf8e0b62f216b9be407
MouseMs <mousems.kuo@gmail.com>
2014-10-21 19:51:53 +0800

    ignore .o  , string structure , input string then output back.

--- hw1.cpp
+++ hw1.cpp
@@ -1,9 +1,27 @@
 #include <iostream> 
 using namespace std; 
 
+struct MyString{
+    char chars[100];
+    int length;
+};
+
+
 int main() { 
-    cout << "Hello! World!\n"; 
-    cout << "哈囉! C++!\n";
-    
+    MyString str;
+    str.length=0;
+    cout << "Hello! Please input string S first>>";
+    char tmp;
+    bool jump=false;
+    while(scanf("%c" , &tmp) && !jump){
+        if(tmp=='\n'){
+            str.chars[str.length]='\0';
+            jump=true;
+        }else{
+            str.chars[str.length] = tmp;
+            str.length++;
+        }
+    }
+    cout << str.chars;
     return 0; 
 }
