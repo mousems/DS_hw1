b339bb9445d4452091aaf138ee28d879b4cc0044
MouseMs <mousems.kuo@gmail.com>
2014-10-21 20:29:30 +0800

    implete a int Stack.

--- hw1.cpp
+++ hw1.cpp
@@ -1,27 +1,77 @@
 #include <iostream> 
 using namespace std; 
 
-struct MyString{
-    char chars[100];
-    int length;
+//============implete a int Stack.==========
+class intStack{
+    private:
+        int num[100];
+        int length;
+    public:
+        void push(int i);
+        int pop();
+        intStack();
 };
 
+intStack::intStack(void){
+    length=0;
+}
 
-int main() { 
-    MyString str;
-    str.length=0;
-    cout << "Hello! Please input string S first>>";
-    char tmp;
-    bool jump=false;
-    while(scanf("%c" , &tmp) && !jump){
-        if(tmp=='\n'){
-            str.chars[str.length]='\0';
-            jump=true;
-        }else{
-            str.chars[str.length] = tmp;
-            str.length++;
-        }
+void intStack::push(int i){
+    if(length==100){
+        cout << "intStack is full.\n";
+    }else{
+        num[length]=i;
+        length++;
     }
-    cout << str.chars;
+}
+int intStack::pop (){
+    if(length==0){
+        cout << "intStack is empty.\n";
+        return -1;
+    }
+    return num[--length];
+}
+//============implete a int Stack.==========
+
+/*
+   class MyString{
+   private:
+   char C[100];
+   int length=0;
+
+   public:
+   int getLength(){
+   return length;
+   }
+   char getString(){
+   char tmp[101];
+   tmp=C;
+   tmp[length]='\n';
+   return tmp;
+   }
+   int find(char *subject) {// return found index , -1 means not found.
+
+   }
+   }
+
+*/
+int main() { 
+    /*
+       MyString str;
+       str.length=0;
+       cout << "Hello! Please input string S first>>";
+       char tmp;
+       bool jump=false;
+       while(scanf("%c" , &tmp) && !jump){
+       if(tmp=='\n'){
+       str.chars[str.length]='\0';
+       jump=true;
+       }else{
+       str.chars[str.length] = tmp;
+       str.length++;
+       }
+       }
+       cout << str.chars;
+       */
     return 0; 
 }
